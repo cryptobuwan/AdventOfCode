b3b39b945293f245e46b68133e7b3dfa05736e1f
cryptobuwan <christopher.carlson7@gmail.com>
2021-12-24 09:47:37 +0000

    trying to join the string into one line then convert string to int then to dec

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -2,8 +2,17 @@
 
 def challenge1(rate):
     for i in range(12):
-        g = [i[bit] for bit in rate if i[bit] == 1]
+        g = ""
+        e = ""
+        if sum(bit[i] == '1' for bit in rate) > len(rate) // 2:
+            g += "1"
+            e += "0"
+        else:
+            e += "1"
+            g += "0"
         print(g)
+        return int(''.join(g), 2) * int(''.join(e), 2)
+
 
 bin_rates = [i for i in open('d3_input')]
 
