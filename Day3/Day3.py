bb72380179d626c5e25e3166d295e02aaa5c35d4
cryptobuwan <christopher.carlson7@gmail.com>
2021-12-27 03:45:32 +0000

    challenge 1 complete

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 
 def challenge1(rate):
+    g = ""
+    e = ""
     for i in range(12):
-        g = ""
-        e = ""
-        if sum(bit[i] == '1' for bit in rate) > len(rate) // 2:
-            g += "1"
-            e += "0"
-        else:
+
+        if sum(bit[i] == '1' for bit in rate) < len(rate) // 2:
             e += "1"
             g += "0"
+        else:
+            g += "1"
+            e += "0"
         print(g)
-        return int(''.join(g), 2) * int(''.join(e), 2)
+    return (int(g, 2) * int(e, 2))
 
 
 bin_rates = [i for i in open('d3_input')]
