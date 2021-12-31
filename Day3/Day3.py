840632a1ae5e687d49ff8fb22daed1005521ef4a
cryptobuwan <christopher.carlson7@gmail.com>
2021-12-31 20:39:33 +0000

    save

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -20,6 +20,22 @@ def challenge1(rate):
     return int(g, 2) * int(e, 2)
 
 
+def challenge2(bits):
+    oxy, co2 = "", ""
+    c1, c0 = 0, 0
+    # create columns for bits
+    for column in range(5):
+        c1 = 0
+        c0 = 0
+        for bit in bits:
+            c1 = bit[column].count(1)
+            c0 = bit[column].count(0)
+
+    return c1, c0
+
+
 bin_rates = [i for i in open('d3_input')]
+demo = [list(map(str.strip, i)) for i in open('sample')]
 
 print("Challenge 1: ", challenge1(bin_rates))
+print("Challenge 2: ", challenge2(demo))
