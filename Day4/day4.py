096146c78cc9718d27adfb3f17a2909594b5fe03
cryptobuwan <christopher.carlson7@gmail.com>
2022-01-04 04:47:58 +0000

    parsing data

--- Day4/day4.py
+++ Day4/day4.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python3
 
+
+with open("d4_input") as f:
+    bingo_nums = [int(x) for x in f.readline().strip('\n').split(',')]
+    bingo_cards = []
+    while f.readlines():
+        bingo_cards = []
+        for i in range(5):
+            bingo_cards.append([int(x) for x in f.readline().strip('\n').split(' ')])
+
+            print(bingo_cards)
